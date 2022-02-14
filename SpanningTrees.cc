af8fef1973420d9e22441aeaa43538d11bc375fc
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-02-14 18:44:25 -0500

    Changed command line argument handling

--- SpanningTrees.cc
+++ SpanningTrees.cc
@@ -45,15 +45,18 @@ float EuclidDist(std::vector<float>, std::vector<float>);
 float GetTreeWeight();
 
 int main(int argc, char** argv) {
-    if (argc != 4) {
-        printf("You used %i arguments\n", argc);
+    if (argc != 5) {
+        printf("Usage: ./randmst 0 numpoints numtrials dimension\n");
         return -1;
     }
 
-    printf("%s\n", argv[0]);
-    printf("%s\n", argv[1]);
-    printf("%s\n", argv[2]);
-    printf("%s\n", argv[3]);
+    long numpoints = std::strtol(argv[2], nullptr, 0);
+    long numtrials = std::strtol(argv[3], nullptr, 0);
+    long dimension = std::strtol(argv[4], nullptr, 0);
+
+    printf("Number of points: %i \n", numpoints);
+    printf("Number of trials: %i \n", numtrials);
+    printf("Dimension: %i \n", dimension);
 
     return 0;
 }
