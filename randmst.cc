afe76ccf06aab31c6f1c2edeb54bf73fae6922ac
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-02-17 19:33:34 -0500

    Fixing segmentation fault

--- randmst.cc
+++ randmst.cc
@@ -4,6 +4,8 @@
 #include <string>
 #include <utility>
 #include <math.h>
+#include <unordered_map>
+#include <random>
 
 using namespace std;
 // =================================================
@@ -29,29 +31,14 @@ using namespace std;
 struct Vertex {
     // We should label them somehow to make it easier to generate
     //      all edges
-    string label;
+    string id;
     vector<float> coords;
 
     // constructor takes as input the node's coordinates
-    Vertex(vector<float> c) {
+    Vertex(string i, vector<float> c = {}) {
         coords = c;
+        id = i;
     }
-};
-
-struct Edge {
-    float weight;
-    vector<Vertex*> vertices;
-
-    // constructor takes as input the edge's connecting nodes and weight
-    Edge(vector<Vertex*> v, float w) {
-        vertices = v;
-        weight = w;
-    }
-};
-
-struct MinSpanningTree {
-    vector<Vertex*> vertices;
-    vector<Edge*> edges;
 };
 
 // Fibonacci Heap structs
@@ -60,14 +47,15 @@ struct node {
     node* child; // Child pointer
     node* left; // Pointer to the node on the left
     node* right; // Pointer to the node on the right
-    float key; // Value of the node
+    float weight; // Value of the node
+    string id; // ID of vertex this represents
     int degree; // Degree of the node
     char mark; // Black or white mark of the node
     char c; // Flag for assisting in the Find node function
 };
 
 // Creating min pointer as "mini"
-struct node* mini = NULL;
+node* mini = NULL;
 
 // TODO:
 // 1. generate all vertices
@@ -78,14 +66,24 @@ struct node* mini = NULL;
     // this value needs to have a pointer to the original edge
 // 5. add edge to min spanning tree
 
-// EudlidDist takes as input the locations/coordinates of two points and returns
-// their Euclidean distance
+// Given a spanning tree as input gets the weight of the tree
+float GetTreeWeight();
+
+// generates random float in [0, 1]
+float GenerateRandFloat();
+
+// calculate euclidDist
 float EuclidDist(vector<float>, vector<float>);
 
-// Given a spanning tree as input gets the weight of the tree
-float GetTreeWeight(MinSpanningTree);
+// generates a single vertex, giving it an ID and (eventually) coordinates
+// TODO: generate coordinates
+Vertex GenerateVertex(string, int);
 
-void GenerateVertice();
+// takes NotS, current vertex in S (call it v), dimension
+//      iterate through NotS
+//          for each node in NotS get edge weight to v
+//          update FibHeap depending on edge weight
+void OptimizeHeap(unordered_map<string, Vertex*>, Vertex, int);
 
 // Fibonacci Heap implemenation
 
@@ -93,10 +91,10 @@ void GenerateVertice();
 int no_of_nodes = 0;
 
 // Function to insert a node in heap
-void insertion(float val);
+void insertion(float weight, string id);
 
 // Linking the heap nodes in parent child relationship
-void Fibonnaci_link(struct node* ptr2, struct node* ptr1);
+void Fibonnaci_link(node* ptr2, node* ptr1);
 
 // Consolidating the heap
 // consolidate root list by continually linking trees until we have at most one binomial tree for a degree k
@@ -105,88 +103,126 @@ void Consolidate();
   
 // Function to extract minimum node in the heap
 // sort the root list with bucket sort according to increasing degree (number of k children directly below node)
-void Extract_min();
+node* Extract_min();
   
 // Cutting a node in the heap to be placed in the root list
-void Cut(struct node* found, struct node* temp);
+void Cut(node* found, node* temp);
   
 // Recursive cascade cutting function
-void Cascase_cut(struct node* temp);
+void Cascase_cut(node* temp);
   
 // Function to decrease the value of a node in the heap
-void Decrease_key(struct node* found, float val);
+void Decrease_weight(node* found, float val);
   
 // Function to find the given node
-void Find(struct node* mini, float old_val, float val);
+void Find(node* mini, float old_val, float val);
   
-// Deleting a node from the heap
-// we can not cut off a child directly under root without changing its order of k
-void Deletion(float val);
   
 // Function to display the heap
 void display();
 
 int main(int argc, char** argv) {
     if (argc != 5) {
-        printf("Usage: ./randmst 0 numpoints numtrials dimension\n");
+        std::printf("Usage: ./randmst 0 numpoints numtrials dimension\n");
         return -1;
     }
-
-    // hashmap S initialized with 1 vertex
-    // hashmap NotS initialized with all other vertices (n-1)
-    
-    // For each vertex not in S we generate "weights"
-
-    // 
-
-    // Build Fibheap
-
-    // extract the minimum
 
     long numpoints = strtol(argv[2], nullptr, 0);
     long numtrials = strtol(argv[3], nullptr, 0);
     long dimension = strtol(argv[4], nullptr, 0);
 
-    printf("Number of points: %li \n", numpoints);
-    printf("Number of trials: %li \n", numtrials);
-    printf("Dimension: %li \n", dimension);
+    float mstWeight = 0;
 
-    cout << "Creating an initial heap" << endl;
-    insertion(5.);
-    insertion(4.1);
-    insertion(8.);
-  
-    // Now we will display the root list of the heap
-    display();
-  
-    // Now we will extract the minimum value node from the heap
-    cout << "Extracting min" << endl;
-    Extract_min();
-    display();
-  
-    // Now we will decrease the value of node '8' to '7'
-    cout << "Decrease value of 8 to 7" << endl;
-    Find(mini, 1.5, 7.);
-    display();
-  
-    // Now we will delete the node '7'
-    cout << "Delete the node 7" << endl;
-    Deletion(7.);
-    display();
+    // vector S initialized with 1 vertex
+    // vector<Vertex*> S;
+    Vertex currSVertex = GenerateVertex("v0", dimension);
+
+    // hashmap NotS initialized with all other vertices (n-1)
+    unordered_map<string, Vertex*> NotS;
+
+    // initialize vertices in S and Not S
+    for (int i=1; i < numpoints; i++) {
+        string id = "v" + to_string(i);
+        Vertex v = GenerateVertex(id, dimension);
+        NotS[id] = &v;
+    }
+
+    // initialize fibonacci heap
+    for (auto entry : NotS) {
+        Vertex notSV = *(entry.second);
+        insertion(GenerateRandFloat(), notSV.id);
+    }
+
+    // while NotS is not empty update FibonacciHeap and get minimum edge
+    while (NotS.empty() == false) {
+        // pass in current version of NotS, last element in S (most recently added vertex) and dimension
+        OptimizeHeap(NotS, currSVertex, dimension);
+        // get minimum FibNode
+        node* minimum = Extract_min();
+        // If the FibHeap is empty break out
+        if (minimum == NULL) {
+            break;
+        }
+        // Add min weight to MST
+        mstWeight += minimum->weight;
+        // Vertex that gets connected to MST from Not S
+        Vertex vToMST = *NotS[minimum->id];
+        currSVertex = vToMST;
+        NotS.erase(vToMST.id);
+    }
+
+    printf("MST weight: %f\n", mstWeight);
 
     return 0;
 }
 
+
+Vertex GenerateVertex(string id, int dim) {
+    if (dim == 0) {
+        Vertex v(id);
+        return v;
+    }
+    else {
+        vector<float> coord(0, 0);
+        Vertex v(id, coord);
+        return v;
+    }
+}
+
+// EudlidDist takes as input the locations/coordinates of two points and returns
+// their Euclidean distance
 float EuclidDist(vector<float>, vector<float>) {
     return 0;
-};
+}
+
+float GenerateRandFloat() {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> dist(0.0, 1.0);
+    double doesntmatter = dist(mt);
+    return (float)rand()/(float)(RAND_MAX/1.0); //
+}
+
+// NotS: vertices currently not in tree
+// SVertex: vertex in S we are currently creating edge weights to
+// dim: dimension is necessary to calculate Euclidean distance between vertices
+void OptimizeHeap(unordered_map<string, Vertex*> NotS, Vertex SVertex, int dim) {
+    for (auto entry : NotS) {
+        Vertex v = *(entry.second);
+        // if dimension is zero generate random weight in [0, 1]
+        if (dim == 0) {
+            float weight = GenerateRandFloat();
+            printf("%f\n", weight);
+        }
+    }
+}
 
 // --- FIBONACCI HEAP IMPLEMENTATION ---- //
 
-void insertion(float val)
-{
-    struct node* new_node = new node();
-    new_node->key = val;
+void insertion(float weight, string id) {
+    node* new_node = new node();
+    new_node->weight = weight;
+    new_node->id = id;
     new_node->degree = 0;
     new_node->mark = 'W';
     new_node->c = 'N';
@@ -194,22 +230,25 @@ void insertion(float val)
     new_node->child = NULL;
     new_node->left = new_node;
     new_node->right = new_node;
-    if (mini != NULL) {
+    // if this is the first node in our FibHeap
+    if (mini == NULL) {
+        mini = new_node;
+    }
+    // If we already have nodes in our FibHeap
+    else {
         (mini->left)->right = new_node;
         new_node->right = mini;
         new_node->left = mini->left;
         mini->left = new_node;
-        if (new_node->key < mini->key)
+        if (new_node->weight < mini->weight)
             mini = new_node;
-    }
-    else {
-        mini = new_node;
+
     }
     no_of_nodes++;
+    printf("%i\n", no_of_nodes);
 }
 
-void Fibonnaci_link(struct node* ptr2, struct node* ptr1)
-{
+void Fibonnaci_link(node* ptr2, node* ptr1) {
     (ptr2->left)->right = ptr2->right;
     (ptr2->right)->left = ptr2->left;
     if (ptr1->right == ptr1)
@@ -223,19 +262,18 @@ void Fibonnaci_link(struct node* ptr2, struct node* ptr1)
     ptr2->left = (ptr1->child)->left;
     ((ptr1->child)->left)->right = ptr2;
     (ptr1->child)->left = ptr2;
-    if (ptr2->key < (ptr1->child)->key)
+    if (ptr2->weight < (ptr1->child)->weight)
         ptr1->child = ptr2;
     ptr1->degree++;
 }
 // Consolidating the heap
 // consolidate root list by continually linking trees until we have at most one binomial tree for a degree k
 // called after extract min
-void Consolidate()
-{
+void Consolidate() {
     int temp1;
     float temp2 = (log(no_of_nodes)) / (log(2));
     int temp3 = temp2;
-    struct node* arr[temp3+1];
+    node* arr[temp3+1];
     for (int i = 0; i <= temp3; i++)
         arr[i] = NULL;
     node* ptr1 = mini;
@@ -247,7 +285,7 @@ void Consolidate()
         temp1 = ptr1->degree;
         while (arr[temp1] != NULL) {
             ptr2 = arr[temp1];
-            if (ptr1->key > ptr2->key) {
+            if (ptr1->weight > ptr2->weight) {
                 ptr3 = ptr1;
                 ptr1 = ptr2;
                 ptr2 = ptr3;
@@ -273,7 +311,7 @@ void Consolidate()
                 arr[j]->right = mini;
                 arr[j]->left = mini->left;
                 mini->left = arr[j];
-                if (arr[j]->key < mini->key)
+                if (arr[j]->weight < mini->weight)
                     mini = arr[j];
             }
             else {
@@ -281,7 +319,7 @@ void Consolidate()
             }
             if (mini == NULL)
                 mini = arr[j];
-            else if (arr[j]->key < mini->key)
+            else if (arr[j]->weight < mini->weight)
                 mini = arr[j];
         }
     }
@@ -289,17 +327,17 @@ void Consolidate()
   
 // Function to extract minimum node in the heap
 // sort the root list with bucket sort according to increasing degree (number of k children directly below node)
-void Extract_min()
-{
-    if (mini == NULL)
-        cout << "The heap is empty" << endl;
+node* Extract_min() {
+    if (mini == NULL) {
+        std::cout << "The heap is empty" << endl;
+        return NULL;
+    }
     else {
         node* temp = mini;
         node* pntr;
         pntr = temp;
         node* x = NULL;
         if (temp->child != NULL) {
-  
             x = temp->child;
             do {
                 pntr = x->right;
@@ -307,7 +345,7 @@ void Extract_min()
                 x->right = mini;
                 x->left = mini->left;
                 mini->left = x;
-                if (x->key < mini->key)
+                if (x->weight < mini->weight)
                     mini = x;
                 x->parent = NULL;
                 x = pntr;
@@ -323,11 +361,13 @@ void Extract_min()
             Consolidate();
         }
         no_of_nodes--;
+        printf("Nodes left: %i\n", no_of_nodes);
+        return temp;
     }
 }
   
 // Cutting a node in the heap to be placed in the root list
-void Cut(struct node* found, struct node* temp)
+void Cut(node* found, node* temp)
 {
     if (found == found->right)
         temp->child = NULL;
@@ -349,7 +389,7 @@ void Cut(struct node* found, struct node* temp)
 }
   
 // Recursive cascade cutting function
-void Cascase_cut(struct node* temp)
+void Cascase_cut(node* temp)
 {
     node* ptr5 = temp->parent;
     if (ptr5 != NULL) {
@@ -364,63 +404,49 @@ void Cascase_cut(struct node* temp)
 }
   
 // Function to decrease the value of a node in the heap
-void Decrease_key(struct node* found, float val)
+void Decrease_weight(node* found, float new_weight)
 {
     if (mini == NULL)
-        cout << "The Heap is Empty" << endl;
+        std::cout << "The Heap is Empty" << endl;
   
     if (found == NULL)
-        cout << "Node not found in the Heap" << endl;
+        std::cout << "Node not found in the Heap" << endl;
   
-    found->key = val;
+    found->weight = new_weight;
   
-    struct node* temp = found->parent;
-    if (temp != NULL && found->key < temp->key) {
+    node* temp = found->parent;
+    if (temp != NULL && found->weight < temp->weight) {
         Cut(found, temp);
         Cascase_cut(temp);
     }
-    if (found->key < mini->key)
+    if (found->weight < mini->weight)
         mini = found;
 }
-  
+
 // Function to find the given node
-void Find(struct node* mini, float old_val, float val)
+void Find(node* mini, string id, float new_weight)
 {
-    struct node* found = NULL;
+    node* found = NULL;
     node* temp5 = mini;
     temp5->c = 'Y';
     node* found_ptr = NULL;
-    if (temp5->key == old_val) {
+    if (temp5->id == id) {
         found_ptr = temp5;
         temp5->c = 'N';
         found = found_ptr;
-        Decrease_key(found, val);
+        float old_weight = temp5->weight;
+        if (new_weight < old_weight) {
+            Decrease_weight(found, new_weight);
+        }
     }
     if (found_ptr == NULL) {
         if (temp5->child != NULL)
-            Find(temp5->child, old_val, val);
+            Find(temp5->child, id, new_weight);
         if ((temp5->right)->c != 'Y')
-            Find(temp5->right, old_val, val);
+            Find(temp5->right, id, new_weight);
     }
     temp5->c = 'N';
     found = found_ptr;
-}
-  
-// Deleting a node from the heap
-void Deletion(float val)
-{
-    if (mini == NULL)
-        cout << "The heap is empty" << endl;
-    else {
-  
-        // Decreasing the value of the node to 0
-        Find(mini, val, 0);
-  
-        // Calling Extract_min function to
-        // delete minimum value node, which is 0
-        Extract_min();
-        cout << "Key Deleted" << endl;
-    }
 }
   
 // Function to display the heap
@@ -428,18 +454,18 @@ void display()
 {
     node* ptr = mini;
     if (ptr == NULL)
-        cout << "The Heap is Empty" << endl;
+        std::cout << "The Heap is Empty" << endl;
   
     else {
-        cout << "The root nodes of Heap are: " << endl;
+        std::cout << "The root nodes of Heap are: " << endl;
         do {
-            cout << ptr->key;
+            std::cout << ptr->weight;
             ptr = ptr->right;
             if (ptr != mini) {
-                cout << "-->";
+                std::cout << "-->";
             }
         } while (ptr != mini && ptr->right != NULL);
-        cout << endl
+        std::cout << endl
              << "The heap has " << no_of_nodes << " nodes" << endl
              << endl;
     }
